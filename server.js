3aef27874ec3700ecbb9882a90ae2bdda2b8ec29
JustPetrov <thepetrovshop@gmail.com>
2026-08-18 09:59:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const DEFAULT_READER_ID = process.env.SUMUP_READER_ID;
 
 // MySQL Database Pool voor locatie-koppelingen
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: '##',
+  password: '##',
+  database: 'bendemen_pos'
 });
 
 const sumupAxios = axios.create({
